b568395a7f7c89b2492fbc86323e5ceef47ecc33
Rajesh <rajesh@Admins-MacBook-Pro.local>
2023-12-14 14:52:31 +0530

    alert box

--- src/App.js
+++ src/App.js
@@ -57,11 +57,27 @@ function App() {
       <div className="form-container">
         <form className="form">
           <div className="inp-container">
-            <input id="accNum" type="text" value={accNum} readOnly />
-            <input id="amount" type="text" value={amount} readOnly />
+            <input
+              id="accNum"
+              type="text"
+              value={`Account Number : ${accNum}`}
+              readOnly
+            />
+
+            <input
+              id="amount"
+              type="text"
+              value={`Amount : ${amount}`}
+              readOnly
+            />
           </div>
           <div>
-            <input id="accHolder" type="text" value={accHolder} readOnly />
+            <input
+              id="accHolder"
+              type="text"
+              value={`Holder Name : ${accHolder}`}
+              readOnly
+            />
           </div>
           <div className="btns">
             <input
